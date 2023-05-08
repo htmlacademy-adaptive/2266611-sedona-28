e3aad4bb8d78af3dfee36737222b145f2a11698f
heligie <heliemzh@gmail.com>
2023-05-08 16:24:20 +0300

    Вносит правки

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ const html = () => {
 const scripts = () => {
   return gulp.src('source/js/*.js')
   .pipe(terser())
+  .pipe(rename({ suffix: '.min' }))
   .pipe(gulp.dest('build/js'))
   .pipe(browser.stream());
 }
